658a1ba201c459247f458c56011ef108777561a7
sameom1048 <samvision99@naver.com>
2025-02-14 11:02:52 +0900

    detail

--- frontend/src/app/post/[id]/page.tsx
+++ frontend/src/app/post/[id]/page.tsx
@@ -1,1 +1,31 @@
-export default async function Page() {}
+import createClient from "openapi-fetch";
+import ClientPage from "./ClientPage";
+import { paths } from "@/lib/backend/apiV1/schema";
+
+const client = createClient<paths>({
+  baseUrl: "http://localhost:8080",
+});
+
+export default async function Page({
+  params,
+}: {
+  params: {
+    id: number;
+  };
+}) {
+  const id = await params.id;
+
+  const response = await client.GET("/api/v1/posts/{id}", {
+    params: {
+      path: {
+        id,
+      },
+    },
+  });
+
+  const rsData = response.data!!;
+
+  const post = rsData.data;
+
+  return <ClientPage post={post} />;
+}
