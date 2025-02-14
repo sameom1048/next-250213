0b61ecc79624bdd6474ef14755b59d4fbbffff83
sameom1048 <samvision99@naver.com>
2025-02-14 10:04:53 +0900

    paging

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,5 +1,7 @@
 import { paths } from "@/lib/backend/apiV1/schema";
+import Link from "next/link";
 import createClient from "openapi-fetch";
+import ClientPage from "./ClientPage";
 
 const client = createClient<paths>({
   baseUrl: "http://localhost:8080",
@@ -11,76 +13,38 @@ export default async function Page({
   searchParams: {
     keywordType?: "title" | "content";
     keyword: string;
+    pageSize: number;
+    page: number;
   };
 }) {
-  const { keywordType = "title", keyword = "" } = await searchParams;
+  const {
+    keywordType = "title",
+    keyword = "",
+    pageSize = 10,
+    page = 1,
+  } = await searchParams;
 
   const response = await client.GET("/api/v1/posts", {
     params: {
       query: {
-        keyword: keyword,
-        keywordType: keywordType,
+        keyword,
+        keywordType,
+        pageSize,
+        page,
       },
     },
   });
 
   const rsData = response.data!!;
-
   const pageDto = rsData.data;
 
-  // const response = await fetch(
-  //   `http://localhost:8080/api/v1/posts?keywordType=${keywordType}&keyword=${keyword}`
-  // );
-
-  // if (!response.ok) {
-  //   throw new Error("에러");
-  // }
-
-  // const rsData = await response.json();
-  // const pageDto: PostItemPageDto = rsData.data;
-
   return (
-    <div>
-      <h1>글 목록</h1>
-
-      <div>응답 코드 : {rsData.code}</div>
-      <div>결과 메시지 : {rsData.msg}</div>
-
-      <div>totalPages : {pageDto.totalPages}</div>
-      <div>totalItems : {pageDto.totalItems}</div>
-      <div>currentPageNo : {pageDto.currentPageNo}</div>
-      <div>pageSize : {pageDto.pageSize}</div>
-
-      <hr />
-
-      <form>
-        <select name="keywordType" defaultValue={keywordType}>
-          <option value="title">제목</option>
-          <option value="content">내용</option>
-        </select>
-        <input
-          placeholder="검색어 입력"
-          type="text"
-          name="keyword"
-          defaultValue={keyword}
-        />
-        <input type="submit" value="검색" />
-      </form>
-
-      <ul>
-        {pageDto.items.map((item) => {
-          return (
-            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
-              <div>id : {item.id}</div>
-              <div>title : {item.title}</div>
-              <div>authorId : {item.authorId}</div>
-              <div>authorName : {item.authorName}</div>
-              <div>published : {`${item.published}`}</div>
-              <div>listed : {`${item.listed}`}</div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ClientPage
+      rsData={rsData}
+      pageSize={pageSize}
+      keyword={keyword}
+      keywordType={keywordType}
+      page={page}
+    />
   );
 }
