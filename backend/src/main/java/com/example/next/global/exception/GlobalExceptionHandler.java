cb4c87918c44dbc25856e5b3eaa939f268e2a1ee
sameom1048 <samvision99@naver.com>
2025-02-14 11:21:55 +0900

    ResponseStatus

--- backend/src/main/java/com/example/next/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/next/global/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.stream.Collectors;
@@ -33,6 +34,7 @@ public class GlobalExceptionHandler {
     }
 
 
+    @ResponseStatus
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<RsData<Void>> ServiceExceptionHandle(ServiceException ex) {
 
