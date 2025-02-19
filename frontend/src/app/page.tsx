11a2a0e519366f0c5ad8c19ddc004961f1ef8ed3
sameom1048 <samvision99@naver.com>
2025-02-19 09:50:45 +0900

    shadcn apply

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div>main page</div>;
+  return (
+    <div>
+      main page
+      <Button className="flex flex gap-3 w-1/4">Click me</Button>
+    </div>
+  );
 }
